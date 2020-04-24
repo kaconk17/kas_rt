ae350391038cf84182bb2dd482379892047f5e44
kaconk <harutonouciha@gmail.com>
2020-04-24 22:38:58 +0700

    seeder user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::create([
+            'id'=> Str::uuid(),
+        'nama' => 'admin2',
+        'email' => 'admin2@gmail.com',
+        'password' => Hash::make('123456'),
+        'phone' => '0033344555',
+       'jenis_kelamin' => 'L',
+        'alamat_asal' => 'Bangil',
+        'alamat_sekarang' => 'Malang',
+       'tgl_lahir' => '1988-05-03',
+        'pekerjaan' => 'Guru',
+        'no_ktp' => '99277788832',
+        'agama' => 'Islam',
+        'level' => 'admin',
+       
+    ]);
+    }
+}
